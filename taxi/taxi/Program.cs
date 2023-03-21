6335f766b134e6a8d23787a3364602fce7c8432a
user <user@DESKTOP-LEPJD0T>
2023-03-21 21:03:51 +0700

    Add project files.

---
+++ taxi/taxi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            tugas taxi = new tugas();
+
+            //pengesetan nilai properties
+            taxi.Drivename = "Rama";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 5;
+
+            //pemanggilan method
+            taxi.Info();
+            taxi.PickUpPassenger();
+            taxi.DropoffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi/taxi/taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    public class tugas
+    {
+        public String Drivename { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void Info()
+        {
+            Console.WriteLine("Nama: {0}", Drivename);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty: yes", OnDuty);
+            }
+            else if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty: No", OnDuty);
+            }
+            Console.WriteLine("NumberPass: {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", Drivename);
+        }
+        public void DropoffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", Drivename);
+        }
+
+    }
+
+}
